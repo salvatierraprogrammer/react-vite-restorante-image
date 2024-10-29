2c514296fdb0a81679f7a1a8f55a8c919fe55ed5
unknown <salvatierraprogrammer@gmail.com>
2024-10-29 03:46:46 -0300

    Backend config

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,21 +1,34 @@
-// Header.js
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import '@fontsource/lobster';
 import logo from '/logo.png';
 import fondoPizza from '/fondoPizza.jpeg';
+import { useNavigate } from 'react-router-dom';
+import SettingsIcon from '@mui/icons-material/Settings'; // Importa un icono de configuración
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleNavigateConfigurar = () => {
+    navigate('/configurar'); // Redirige a la ruta /configurar
+  };
+
   return (
     <header style={{ 
       backgroundImage: `url(${fondoPizza})`, 
       backgroundSize: 'cover',
       backgroundBlendMode: 'overlay',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Añade un color de fondo semitransparente para el degradado
+      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Color de fondo semitransparente
       padding: '30px 0', 
       borderBottom: '5px solid #d35400',
       boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
     }}>
+      <Button 
+        onClick={handleNavigateConfigurar} 
+       
+      >.
+      </Button>
+
       <Typography
         variant="h3"
         gutterBottom
@@ -27,8 +40,9 @@ function Header() {
           marginBottom: '10px',
         }}
       >
-        Pizzería Felipe
+        Pizzería Felipe.
       </Typography>
+
       <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
         <div style={{ 
           backgroundColor: '#fff', 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,15 @@ import { Container } from '@mui/material';
 import ButtonSubir from '../component/ButtonSubir';
 import ButtonDelivery from '../component/ButtonDelivery';
 
+
 function Home() {
+
   return (
     <Container style={{ padding: 0, backgroundColor: '#f7f1e3', minHeight: '100vh' }} disableGutters>
       <ListMenu />
       <ButtonDelivery />
       <ButtonSubir />
+    
     </Container>
   );
 }
