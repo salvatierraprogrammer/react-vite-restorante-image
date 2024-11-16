2d9715aa768a7d94daa4225d4df42b5c59af751e
unknown <salvatierraprogrammer@gmail.com>
2024-11-15 23:38:03 -0300

    Resolviendo conflictos Al refrescar configurar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Abre el navegador automáticamente
+  },
+  build: {
+    outDir: 'dist', // Directorio de salida para la construcción
+  },
 })
+
